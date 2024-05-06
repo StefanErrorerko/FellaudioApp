57d24c01c6c2c6a541da9a02d4f6df72163aec75
StefanErrorerko <nashes1320@gmail.com>
2024-05-06 15:22:33 +0300

    deleted4

--- fellaudio-react-app/src/App.js
+++ fellaudio-react-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+//import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Map from './pages/Map'
+import Content from './pages/Content'
+import Profile from './pages/Profile'
+import Playlist from './pages/Playlist'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path='/' element = {<Home />} />
+          <Route path='/about' element = {<About />} />
+          <Route path='/login' element = {<Login />} />
+          <Route path="/content" element={<Content />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/playlist" element={<Playlist />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
