33f575ba0b3734caff330c3f7ba7379714897f72
StefanErrorerko <nashes1320@gmail.com>
2024-05-06 14:21:29 +0300

    deleted2

--- fellaudio-react-app/src/App.js
+++ fellaudio-react-app/src/App.js
@@ -1,37 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/Navbar';
-//import Footer from './components/Footer'
-import Home from './pages/Home'
-//import Menu from './pages/Menu'
-import About from './pages/About'
-import Login from './pages/Login'
-import Map from './pages/Map'
-import Content from './pages/Content'
-import Profile from './pages/Profile'
-import Playlist from './pages/Playlist'
-import Test from './pages/Test'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path='/' element = {<Home />} />
-          <Route path='/about' element = {<About />} />
-          <Route path='/login' element = {<Login />} />
-          <Route path="/content" element={<Content />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/playlist" element={<Playlist />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
